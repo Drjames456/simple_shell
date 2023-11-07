8ced33aed86ce4ee556fb03b3235942fa43b2595
Osunba Adeola <osunbaadeola56@gmail.com>
2023-11-07 10:48:11 -0800

    Simple Shell

---
+++ makebettyproud.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+
+/**
+ * main - this is main function
+ *
+ * Return: Always 0
+ */
+
+
+
+
+int main(void)
+{
+	int i = 2;
+	int j = 1;
+	int k;
+
+	for (j = 1; j <= 12; j++)
+	{
+		k = i * j;
+		printf("%d\n", k);
+	}
+	return (0);
+}
